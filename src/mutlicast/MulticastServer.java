effc560ae37c66352e41aac0ea11e0fed997885b
Bdenman508 <55107256+Bdenman508@users.noreply.github.com>
2023-11-06 16:42:46 -0800

    init commit

---
+++ src/mutlicast/MulticastServer.java
@@ -0,0 +1,10 @@
+package mutlicast;
+
+public class MulticastServer {
+
+	public static void main(String[] args) throws java.io.IOException {
+		//TODO Auto-generated method stub
+		new MulticastServerThread().start();
+				
+	}
+}
